15f3944c17ae1cab1b78364c4c98063d98d29b0d
Darius Kibagendi <dariuskibagendi@yahoo.com>
2024-06-18 01:21:07 +0600

    Submiting my PLP assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('What is you name: ')
+age = input('What is your age: ')
+location = input('Where do you come from: ')
+print("Hello "+ name + " you are "+ age + " years old, coming from "+ location)
